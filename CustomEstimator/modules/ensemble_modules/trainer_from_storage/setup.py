a948255ac2b603e8f31a33884731b152876142a2
mekhod <mekhod@gmail.com>
2018-11-06 13:17:23 -0600

    Replace glob by tf.gfile.Glob

--- CustomEstimator/modules/ensemble_modules/trainer_from_storage/setup.py
+++ CustomEstimator/modules/ensemble_modules/trainer_from_storage/setup.py
@@ -2,16 +2,11 @@ from setuptools import find_packages
 from setuptools import setup
 
 REQUIRED_PACKAGES = ['argparse',
-                     'tensorflow==1.10.0',
-                     'glob3==0.0.1',
-                     'os',
-                     'sklearn==1.19.2',
-                     'numpy==1.14.5',
-                     'pandas==0.23.4',
-                     'MultiColProcessor==1.0.15',
-                     'pickle',
-                     'json',
-                     'collections',
+                     'tensorflow>=1.10.0',
+                     'sklearn',
+                     'numpy>=1.14.5',
+                     'pandas>=0.23.4',
+                     'MultiColProcessor',
                      'multiprocessing']
 
 setup(
