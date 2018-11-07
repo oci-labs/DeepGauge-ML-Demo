a3934c009254fd411007e45a1127c8e354f048d8
mekhod <mekhod@gmail.com>
2018-11-07 08:52:26 -0600

    Split dataprocessing

--- CustomEstimator/modules/ensemble_modules/trainer_from_storage/trainer_original/input.py
+++ CustomEstimator/modules/ensemble_modules/trainer_from_storage/trainer_original/input.py
@@ -3,93 +3,20 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 import pandas as pd
 # from MultiColProcessor import MultiColProcessor as mcp
+import MultiColProcessor as mcp
 import pickle
 import json
 from collections import OrderedDict
 import tensorflow as tf
 import multiprocessing
 import os
-from sklearn import preprocessing
 
 
-##
-class MultiColomnOneHotEncoder:
-    ##
-    def __init__(self):
-        self.__catColumns = []
-        self.__MultiOHE = {}
-
-    ##
-    def __getCategoryColomns(self, data=pd.DataFrame()):
-        catColumns = []
-        for i, j in enumerate(data):
-            if (data.dtypes[i].name == 'category'):
-                catColumns.append(j)
-            else:
-                continue
-        ##
-        self.__catColumns = catColumns
-        ##
-        return
-
-    ##
-    def fit(self, data):
-        ##
-        self.__getCategoryColomns(data)
-        ##
-        for col in self.__catColumns:
-            OneHotEncoder = preprocessing.OneHotEncoder(sparse=False)
-            OneHotEncoder.fit(np.array(data.loc[:, col]).reshape(-1, 1))
-            self.__MultiOHE[col] = OneHotEncoder
-        ##
-        return self
-
-    def transform(self, data):
-
-        ##
-        catData = data[self.__catColumns]
-        data = data.drop(self.__catColumns, axis=1)
-
-        ##
-        def Transform_Rec(dta=catData):
-            ##
-            nCol = dta.shape[1]
-            ##
-            if nCol == 1:
-                ##
-                col = dta.columns[0]
-                OneHotEncoder = self.__MultiOHE[col]
-                transformed = OneHotEncoder.transform(np.array(dta.loc[:, col]).reshape(-1, 1))
-                transformed = pd.DataFrame(transformed)
-                transformed.columns = [str(col) + '_' + str(c) for c in transformed.columns]
-                ##
-                return transformed
-
-            else:
-                ##
-                if (nCol % 2 == 0):
-                    middle_index = int(nCol / 2)
-                else:
-                    middle_index = int(nCol / 2 - 0.5)
-                ##
-                left = dta.iloc[:, :middle_index]
-                right = dta.iloc[:, middle_index:]
-                ##
-                return pd.concat([Transform_Rec(dta=left), Transform_Rec(dta=right)], axis=1)
-
-        ##
-        transformedCatData = Transform_Rec(dta=catData)
-        transformedCatData.set_index(data.index, inplace=True)
-
-        ##
-        return pd.concat([data, transformedCatData], axis=1)
-
-###################################
 class Dataset():
     @staticmethod
     def dump_pkl_MultiColProcessor(gauge_categories, bin_path):
         y_true_cat = pd.DataFrame(data=gauge_categories, columns=['y_true'], dtype='category')
-        MultiColumnOneHotEncoder = MultiColomnOneHotEncoder()
+        MultiColumnOneHotEncoder = mcp.MultiColomnOneHotEncoder()
         MultiColumnOneHotEncoder.fit(data=y_true_cat)
         with open(os.path.join(bin_path, 'MultiColProcessor.pkl'), 'wb') as pklFile:
             pickle.dump(MultiColumnOneHotEncoder, pklFile)
