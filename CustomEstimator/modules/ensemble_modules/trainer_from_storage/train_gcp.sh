6ac01f380818aaa11cee1dadea0fb1fe72602ac8
mekhod <mekhod@gmail.com>
2018-11-16 14:05:05 -0600

    Merge master

--- CustomEstimator/modules/ensemble_modules/trainer_from_storage/train_gcp.sh
+++ CustomEstimator/modules/ensemble_modules/trainer_from_storage/train_gcp.sh
@@ -1,6 +1,6 @@
 ##
 REGION=us-central1
-JOB_NAME=custom_estimator_train_base64_trial_132
+JOB_NAME=custom_estimator_train_base64_trial_134
 BUCKET="gs://custom_estimator"
 ##
 PACKAGE_PATH=/home/khodayarim/PycharmProjects/DeepGauge-ML-Demo/CustomEstimator/modules/ensemble_modules/trainer_from_storage/trainer
