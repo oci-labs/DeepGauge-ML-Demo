b8ce34719110c0d2303dafe88508659529d465a9
mekhod <mekhod@gmail.com>
2018-11-16 15:42:39 -0600

    Add prediction label to prediction output

--- CustomEstimator/modules/ensemble_modules/trainer_from_storage/predict_base64_locally.py
+++ CustomEstimator/modules/ensemble_modules/trainer_from_storage/predict_base64_locally.py
@@ -1,5 +1,4 @@
 import tensorflow as tf
-import numpy as np
 
 filename = '/home/khodayarim/PycharmProjects/data/ImageEveryUnit/psi_1/gauge_scale_5.jpg'
 def parse_image(filename):
@@ -11,7 +10,7 @@ with tf.Session() as sess:
     image = parse_image(filename=filename)
     image = sess.run(image)
 
-export_dir='/home/khodayarim/PycharmProjects/DeepGauge-ML-Demo/CustomEstimator/modules/ensemble_modules/trainer_from_storage/misc/exported_model/1542398378/'
+export_dir='/home/khodayarim/PycharmProjects/DeepGauge-ML-Demo/CustomEstimator/modules/ensemble_modules/trainer_from_storage/misc/exported_model/1542403650/'
 predict_fn = tf.contrib.predictor.from_saved_model(export_dir)
 
 a = predict_fn({'img_bytes':[image]})
