bd7b6fe2a2edb356e01d1bc08fe78c40ae26f9c6
Jordan Walker <jorwalk@gmail.com>
2018-11-20 11:36:54 -0600

    create config.py to include connexion

---
+++ app-engine/config.py
@@ -0,0 +1,32 @@
+import os
+import connexion
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# Create the connexion application instance
+connex_app = connexion.App(__name__, specification_dir=basedir)
+
+# Get the underlying Flask app instance
+app = connex_app.app
+
+# Build the Sqlite ULR for SqlAlchemy
+sqlite_url = "sqlite:////" + os.path.join(basedir, "deepgauge.db")
+
+# Configure the SqlAlchemy part of the app instance
+app.config["SQLALCHEMY_ECHO"] = True
+app.config["SQLALCHEMY_DATABASE_URI"] = sqlite_url
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+# Configure the following environment variables via app.yaml
+# This is used in the push request handler to veirfy that the request came from
+# pubsub and originated from a trusted source.
+app.config['PUBSUB_VERIFICATION_TOKEN'] = os.environ['PUBSUB_VERIFICATION_TOKEN']
+app.config['PUBSUB_TOPIC'] = os.environ['PUBSUB_TOPIC']
+app.config['PROJECT'] = os.environ['GOOGLE_CLOUD_PROJECT']
+
+# Create the SqlAlchemy db instance
+db = SQLAlchemy(app)
+
+# Initialize Marshmallow
+ma = Marshmallow(app)
