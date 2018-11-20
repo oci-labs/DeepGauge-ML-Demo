990b7833c7fe2091f3ecc6575f88f177996eca35
Jordan Walker <jorwalk@gmail.com>
2018-11-20 16:07:44 -0600

    Update models.py

--- app-engine/models.py
+++ app-engine/models.py
@@ -2,14 +2,14 @@ from datetime import datetime
 from config import db, ma
 
 class Settings(db.Model):
-    __tablename__ = "person"
+    __tablename__ = "settings"
     id = db.Column(db.Integer, primary_key=True)
     type = db.Column(db.String(32))
     frame = db.Column(db.Integer)
     refresh = db.Column(db.Integer)
 
 
-class SettingsSchema(ma.ModelSchema):
+class SettingSchema(ma.ModelSchema):
     class Meta:
         model = Settings
         sqla_session = db.session
