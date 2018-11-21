c9d6eaded5e8bc5dd4c5070d282e9e7a3fd101fc
Jordan Walker <jorwalk@gmail.com>
2018-11-20 23:07:47 -0600

    Update models.py

--- app-engine/models.py
+++ app-engine/models.py
@@ -7,9 +7,74 @@ class Settings(db.Model):
     type = db.Column(db.String(32))
     frame = db.Column(db.Integer)
     refresh = db.Column(db.Integer)
-
-
 class SettingSchema(ma.ModelSchema):
     class Meta:
         model = Settings
+        sqla_session = db.session
+###
+class Reading(db.Model):
+    __tablename__ = "default"
+    id = db.Column(db.Integer, primary_key=True)
+    id_device = db.Column(db.Integer,db.ForeignKey('device.id'),nullable=False)
+    prediction = db.Column(db.String(64))
+    accuracy = db.Column(db.String(64))
+    body = db.Column(db.String(128))
+    timestamp = db.Column(
+        db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow
+    )
+class ReadingSchema(ma.ModelSchema):
+    class Meta:
+        model = Reading
+        sqla_session = db.session
+###
+class Default(db.Model):
+    __tablename__ = "default"
+    id = db.Column(db.Integer, primary_key=True)
+    id_user = db.Column(db.Integer,db.ForeignKey('user.id'),nullable=False)
+    type = db.Column(db.String(32))
+    frame_rate = db.Column(db.String(32))
+    refresh_rate = db.Column(db.String(32))
+    updated = db.Column(
+        db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow
+    )
+class DefaultSchema(ma.ModelSchema):
+    class Meta:
+        model = Default
+        sqla_session = db.session
+###
+class Device(db.Model):
+    __tablename__ = "device"
+    id = db.Column(db.Integer, primary_key=True)
+    id_user = db.Column(db.Integer,db.ForeignKey('user.id'),nullable=False)
+    name = db.Column(db.String(32))
+    image = db.Column(db.String(32))
+    bucket = db.Column(db.String(32))
+    type = db.Column(db.String(32))
+    location = db.Column(db.String(32))
+    frame_rate = db.Column(db.String(32))
+    refresh_rate = db.Column(db.String(32))
+    notes = db.Column(db.String(64))
+    high_threshold = db.Column(db.Integer)
+    low_threshold = db.Column(db.Integer)
+    updated = db.Column(
+        db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow
+    )
+class DeviceSchema(ma.ModelSchema):
+    class Meta:
+        model = Device
+        sqla_session = db.session
+###
+class User(db.Model):
+    __tablename__ = "user"
+    id = db.Column(db.Integer, primary_key=True)
+    user_name = db.Column(db.String(32), nullable=False)
+    display_name = db.Column(db.String(32))
+    company = db.Column(db.String(32))
+    thumbnail = db.Column(db.String(32))
+    updated = db.Column(
+        db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow
+    )
+class UserSchema(ma.ModelSchema):
+    class Meta:
+        model = User
         sqla_session = db.session
