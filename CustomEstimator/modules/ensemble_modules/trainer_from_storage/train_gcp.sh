783b3fe025352c423d81776916d4973b89182d36
mekhod <mekhod@gmail.com>
2018-11-28 15:32:25 -0600

    Add confusion matrix

--- CustomEstimator/modules/ensemble_modules/trainer_from_storage/train_gcp.sh
+++ CustomEstimator/modules/ensemble_modules/trainer_from_storage/train_gcp.sh
@@ -1,6 +1,6 @@
 ##
 REGION=us-central1
-JOB_NAME=custom_estimator_demo_2
+JOB_NAME=custom_estimator_trial_1
 BUCKET="gs://custom_estimator"
 ##
 PACKAGE_PATH="/home/khodayarim/PycharmProjects/DeepGauge-ML-Demo/CustomEstimator/modules/ensemble_modules/trainer_from_storage/trainer"
@@ -9,13 +9,8 @@ PRIMARY_PATH="${BUCKET}/misc/primary_models"
 ENSEMBLE_PATH="${BUCKET}/misc/ensemble_graph"
 IMG_PATH="${BUCKET}/data"
 BIN_PATH="${BUCKET}/misc/logs/dumps"
-STAGING_BUCKET="${BUCKET}/staging_folder"
-# EXPORT_PATH="${BUCKET}/misc/exported_model"
+STAGING_BUCKET="gs://custom_estimator_staging_bucket"
 JOB_DIR="${BUCKET}/misc/logs/job_dir/${JOB_NAME}"
-
-## --job-dir "$JOB_DIR" \
-## --python-version 3.5 \
-## --export_dir="${EXPORT_PATH}" \
 
 gcloud ml-engine jobs submit training "$JOB_NAME" \
     --stream-logs \
@@ -29,8 +24,8 @@ gcloud ml-engine jobs submit training "$JOB_NAME" \
     --primary_models_directory="${PRIMARY_PATH}" \
     --ensemble_architecture_path="${ENSEMBLE_PATH}" \
     --path_to_images="${IMG_PATH}" \
-    --dev=False \
-    --train_epochs=250 \
+    --dev=True \
+    --train_epochs=10 \
     --batch_size=200 \
     --retrain_primary_models=True \
     --learning_rate=6e-4 \
--- CustomEstimator/modules/ensemble_modules/trainer_from_storage/tune_hyper_params.sh
+++ CustomEstimator/modules/ensemble_modules/trainer_from_storage/tune_hyper_params.sh
@@ -1,6 +1,6 @@
 ##
 REGION=us-central1
-JOB_NAME=custom_estimator_demo_2
+JOB_NAME=custom_estimator_tune_hyperParams_21
 BUCKET="gs://custom_estimator"
 ##
 PACKAGE_PATH="/home/khodayarim/PycharmProjects/DeepGauge-ML-Demo/CustomEstimator/modules/ensemble_modules/trainer_from_storage/trainer"
@@ -10,9 +10,8 @@ PRIMARY_PATH="${BUCKET}/misc/primary_models"
 ENSEMBLE_PATH="${BUCKET}/misc/ensemble_graph"
 IMG_PATH="${BUCKET}/data"
 BIN_PATH="${BUCKET}/misc/logs/dumps"
-STAGING_BUCKET="${BUCKET}/staging_folder"
+STAGING_BUCKET="gs://custom_estimator_staging_bucket"
 JOB_DIR="${BUCKET}/misc/logs/job_dir/${JOB_NAME}"
-
 
 gcloud ml-engine jobs submit training "$JOB_NAME" \
     --stream-logs \
@@ -27,8 +26,7 @@ gcloud ml-engine jobs submit training "$JOB_NAME" \
     --primary_models_directory="${PRIMARY_PATH}" \
     --ensemble_architecture_path="${ENSEMBLE_PATH}" \
     --path_to_images="${IMG_PATH}" \
-    --dev=False \
-    --train_epochs=250 \
+    --train_epochs=300 \
     --retrain_primary_models=True \
     --job_dir="$JOB_DIR" \
 
